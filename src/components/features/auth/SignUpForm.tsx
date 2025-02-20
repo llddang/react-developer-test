ff52536cd5f7664090078e8e4a9bd2a0d9215829
llddang <bodyness@naver.com>
2025-02-20 16:17:24 +0900

    feat:#5 SIgnUpForm에 로직 적용

--- src/components/features/auth/SignUpForm.tsx
+++ src/components/features/auth/SignUpForm.tsx
@@ -1,14 +1,52 @@
 import Button from "@/components/commons/Button";
-import Input from "@/components/commons/Input";
+import SignUpInput from "@/components/features/auth/SignUpInput";
+import useSignUpForm from "@/libs/hooks/useSignUpForm";
 
 export default function SignUpForm() {
+  const {
+    formData,
+    errorMessage,
+    changeHandler,
+    focusOutHandler,
+    submitHandler,
+  } = useSignUpForm();
+
   return (
-    <form className="space-y-4 bg-gray-50 p-4 rounded-lg shadow-md">
-      <Input type="text" placeholder="아이디" />
-      <Input type="password" placeholder="비밀번호" />
-      <Input type="text" placeholder="닉네임" />
+    <form
+      onSubmit={submitHandler}
+      className="space-y-4 bg-gray-50 p-4 rounded-lg shadow-md"
+    >
+      <SignUpInput
+        type="text"
+        name="id"
+        value={formData.id}
+        onChange={changeHandler}
+        onBlur={focusOutHandler}
+        placeholder="아이디"
+        autoComplete="username"
+        errorMessage={errorMessage.id}
+      />
+      <SignUpInput
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={changeHandler}
+        onBlur={focusOutHandler}
+        placeholder="비밀번호"
+        autoComplete="new-password"
+        errorMessage={errorMessage.password}
+      />
+      <SignUpInput
+        type="text"
+        name="nickname"
+        value={formData.nickname}
+        onChange={changeHandler}
+        onBlur={focusOutHandler}
+        placeholder="닉네임"
+        errorMessage={errorMessage.nickname}
+      />
       <Button type="submit" className="w-full">
-        로그인
+        회원가입
       </Button>
     </form>
   );
