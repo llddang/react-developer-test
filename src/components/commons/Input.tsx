283abeb822ca0cc4e2e9b1a01a0295ffecf3ea08
llddang <bodyness@naver.com>
2025-02-20 16:11:39 +0900

    feat:#5 input 수정

--- src/components/commons/Input.tsx
+++ src/components/commons/Input.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  errorMessage?: string;
+}
 
-export default function Input({ ...props }: InputProps) {
+export default function Input({ type, errorMessage, ...props }: InputProps) {
   return (
-    <input
-      {...props}
-      className="h-9 bg-white w-full rounded-md border border-zinc-200 px-3 py-2 text-base placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50"
-    />
+    <div>
+      <input
+        type={type}
+        {...props}
+        className={`h-9 bg-white w-full rounded-md border px-3 py-2 text-base placeholder:text-zinc-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-zinc-950 disabled:cursor-not-allowed disabled:opacity-50 
+          ${errorMessage ? "border-red-400" : "border-zinc-200"}`}
+      />
+      {errorMessage && (
+        <p className="text-sm pl-2 text-red-400">{errorMessage}</p>
+      )}
+    </div>
   );
 }
