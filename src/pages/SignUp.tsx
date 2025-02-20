d506b12708688f37c9bfd75353069ead2376ea57
llddang <bodyness@naver.com>
2025-02-20 14:58:15 +0900

    feat:#3 로그인/회원가입 페이지 UI 구현

---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,12 @@
+import SignUpCaption from "@/components/features/auth/SignUpCaption";
+import SignUpForm from "@/components/features/auth/SignUpForm";
+
+export default function SignUp() {
+  return (
+    <div className="bg-white max-w-md w-full mx-auto p-8 shadow-lg rounded-lg space-y-4">
+      <h1 className="text-2xl font-bold">회원가입</h1>
+      <SignUpForm />
+      <SignUpCaption />
+    </div>
+  );
+}
