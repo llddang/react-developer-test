a73ca9aa27246833821c92140a664fb180ea7254
llddang <bodyness@naver.com>
2025-02-21 16:28:44 +0900

    refactor:#13 button과 buttonlink의 cva 공통 분리

--- src/components/commons/ButtonLink.tsx
+++ src/components/commons/ButtonLink.tsx
@@ -1,35 +1,8 @@
-import { cva, VariantProps } from "class-variance-authority";
+import { buttonVariants } from "@/styles/button.cva";
+import { VariantProps } from "class-variance-authority";
 import { Link, LinkProps } from "react-router-dom";
 
-const buttonLinkVariants = cva(
-  "whitespace-nowrap rounded-md disabled:pointer-events-none disabled:opacity-50",
-  {
-    variants: {
-      variant: {
-        default:
-          "bg-primary text-white hover:bg-primary/80 active:bg-primary/90",
-        outline:
-          "border border-primary text-primary hover:text-white bg-white hover:bg-primary/30 active:bg-primary/10",
-        secondary:
-          "bg-secondary text-white hover:bg-secondary/80 active:bg-secondary/90",
-        ghost: "hover:bg-primary/10 text-primary",
-        link: "text-primary underline-offset-4 hover:underline",
-      },
-      size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 px-3 text-xs",
-        lg: "h-10 px-8",
-        icon: "h-9 w-9",
-      },
-    },
-    defaultVariants: {
-      variant: "default",
-      size: "default",
-    },
-  }
-);
-
-type ButtonLinkProps = VariantProps<typeof buttonLinkVariants> & LinkProps;
+type ButtonLinkProps = VariantProps<typeof buttonVariants> & LinkProps;
 export default function ButtonLink({
   variant,
   size,
@@ -40,7 +13,7 @@ export default function ButtonLink({
   return (
     <Link
       to={to}
-      className={buttonLinkVariants({ variant, size, className })}
+      className={buttonVariants({ variant, size, className })}
       {...props}
     />
   );
