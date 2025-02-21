86627838431f08a8ec7d3e6be2888100726bc11a
llddang <bodyness@naver.com>
2025-02-21 20:50:11 +0900

    fix:#15 build 에러 해결을 위해 잠시 console.log 선언

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,9 +1,10 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useLocation } from "react-router-dom";
 
 export default function ResultPage() {
   const location = useLocation();
   const { answers } = location.state;
+
+  console.log(answers);
 
   return <h1>result</h1>;
 }
