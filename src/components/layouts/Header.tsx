768b0b445caf920b044fc99d5e1b2c7fc6580a84
llddang <bodyness@naver.com>
2025-02-21 11:03:58 +0900

    feat:#11 header ui 구현

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -1,3 +1,36 @@
+import Button from "@/components/commons/Button";
+import StyledLink from "@/components/commons/StyledLink";
+
 export default function Header() {
-  return <header className="h-15 !p-4 shadow-md">header...</header>;
+  const isAuth = true;
+  return (
+    <header className="h-15 p-4 shadow-md">
+      <nav className="flex justify-between items-center mx-4 lg:max-w-5xl lg:mx-auto">
+        <StyledLink to="/">홈</StyledLink>
+        {isAuth ? <AuthorizedSession /> : <UnauthorizedSession />}
+      </nav>
+    </header>
+  );
+}
+
+function AuthorizedSession() {
+  return (
+    <div className="relative flex items-center pr-26 gap-4">
+      <StyledLink to="/profile">프로필</StyledLink>
+      <StyledLink to="/test">테스트</StyledLink>
+      <StyledLink to="/results">결과 보기</StyledLink>
+      <Button className="absolute right-0 top-1/2 -translate-y-1/2">
+        로그아웃
+      </Button>
+    </div>
+  );
+}
+
+function UnauthorizedSession() {
+  return (
+    <div>
+      <StyledLink to="/sign-in">로그인</StyledLink> /
+      <StyledLink to="/sign-up">회원가입</StyledLink>
+    </div>
+  );
 }
