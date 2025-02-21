11096299099b23bd26a6feb661c8d42977aaa919
llddang <bodyness@naver.com>
2025-02-21 10:14:43 +0900

    feat:#9 프로필 수정 및 정보 받아오는 api 구현

--- src/libs/api/useAuth.api.ts
+++ src/libs/api/useAuth.api.ts
@@ -1,6 +1,14 @@
+import { QueryKeys } from "@/constants/query-key.constant";
 import { authServer } from "@/libs/api/authServer.axios";
-import { SignInDto, SignInResponseDto, SignUpDto } from "@/types/dto/auth.dto";
-import { useMutation } from "@tanstack/react-query";
+import { queryClient } from "@/main";
+import {
+  ProfileDto,
+  SignInDto,
+  SignInResponseDto,
+  SignUpDto,
+  UserDto,
+} from "@/types/dto/auth.dto";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
 export function useSignUpMutate() {
   return useMutation({
@@ -17,12 +25,38 @@ export function useSignInMutate() {
         "/login",
         signInData
       );
-      const { success: isAuth, nickname, userId, avatar } = response.data;
-      localStorage.setItem("authToken", response.data.accessToken);
-      localStorage.setItem(
-        "user",
-        JSON.stringify({ isAuth, nickname, userId, avatar })
-      );
+      return response.data;
+    },
+    onSuccess: (response) => {
+      const { accessToken, ...auth } = response;
+      localStorage.setItem("authToken", accessToken);
+      localStorage.setItem("user", JSON.stringify(auth));
+    },
+  });
+}
+
+export function useProfileQuery() {
+  return useQuery({
+    queryKey: QueryKeys.MEMBER_ME,
+    queryFn: async (): Promise<UserDto> => {
+      const response = await authServer.get<UserDto>("/user");
+      return response.data;
+    },
+  });
+}
+
+export function useProfileMutate() {
+  return useMutation({
+    mutationFn: async (profileData: ProfileDto): Promise<ProfileDto> => {
+      const formData = new FormData();
+      if (profileData.avatar) formData.append("avatar", profileData.avatar);
+      formData.append("nickname", profileData.nickname);
+
+      const response = await authServer.patch("/profile", formData);
+      return response.data;
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: QueryKeys.MEMBER_ME });
     },
   });
 }
