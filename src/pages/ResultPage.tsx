7ee45143e44bb6218d8bf67463d95c61cea8eda4
llddang <bodyness@naver.com>
2025-02-22 22:15:43 +0900

    refactor:#19 결과 페이지에 컴포넌트 적용

--- src/pages/ResultPage.tsx
+++ src/pages/ResultPage.tsx
@@ -1,5 +1,7 @@
 import Button from "@/components/commons/Button";
 import ButtonLink from "@/components/commons/ButtonLink";
+import ResultMatchCard from "@/components/features/test/ResultMatchCard";
+import ResultMyTypeCard from "@/components/features/test/ResultMyTypeCard";
 import { calculateDeveloperType } from "@/libs/utils/question.utils";
 import { useLocation } from "react-router-dom";
 
@@ -25,44 +27,23 @@ export default function ResultPage() {
   return (
     <div className="bg-white p-8 rounded-lg shadow-lg max-w-4xl mx-auto">
       <h3 className="text-2xl font-semibold">{developType.name} 개발자</h3>
-      <div className="flex flex-col md:flex-row items-center justify-center gap-4 mb-4 md:mb-8">
-        <img
-          src={developType.img}
-          alt={`${developType.name} 이미지`}
-          className="w-4/5 max-w-[300px]"
+      <ResultMyTypeCard
+        name={developType.name}
+        img={developType.img}
+        description={developType.description}
+      />
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-4 items-end justify-items-center">
+        <ResultMatchCard
+          matchTitle="나의 짝꿍"
+          name={developType.goodMatch.name}
+          img={developType.goodMatch.img}
         />
-        <ul className="text-sm md:text-base">
-          {developType.description.map((info) => (
-            <li key={info} className="my-2 ml-6 list-disc">
-              {info}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="flex flex-wrap justify-around items-end mx-4">
-        <div className="p-4 w-fit flex flex-col items-center border border-primary rounded-lg">
-          <p className="font-semibold">나의 짝꿍</p>
-          <img
-            src={developType.goodMatch.img}
-            alt={developType.goodMatch.name}
-            className="w-full max-w-[120px]"
-          />
-          <p className="text-xs sm:text-base mt-2">
-            {developType.goodMatch.name}
-          </p>
-        </div>
-        <div className="p-4 w-fit flex flex-col items-center  border border-primary rounded-lg">
-          <p className="font-semibold">나와 상극</p>
-          <img
-            src={developType.badMatch.img}
-            alt={developType.badMatch.name}
-            className="w-full max-w-[120px]"
-          />
-          <p className="text-xs sm:text-base  mt-2">
-            {developType.badMatch.name}
-          </p>
-        </div>
-        <div className="flex sm:flex-col gap-4 mt-4">
+        <ResultMatchCard
+          matchTitle="나와 상극"
+          name={developType.badMatch.name}
+          img={developType.badMatch.img}
+        />
+        <div className="col-span-2 md:col-span-1 flex flex-wrap md:flex-col w-fit gap-4">
           <ButtonLink to="/test">테스트 다시 하기</ButtonLink>
           <Button variant="outline" onClick={handleShareLinkClick}>
             테스트 공유 하기
